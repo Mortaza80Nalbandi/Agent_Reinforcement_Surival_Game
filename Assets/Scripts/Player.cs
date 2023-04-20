656cee78c5785107ea382c0b0ad9420ec57a9cb0
Mortaza Nalbandi <mortazana8@gmail.com>
2023-04-20 10:12:59 +0330

    player added and movement added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //private int health;
+    //private int damage;
+    private float speed;
+    private int up,down,left,right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.6f;
+        //health = 100;
+        //damage = 5;
+        up = 0;
+        down = 0;
+        left =0 ;
+        right = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        
+    }
+    private void FixedUpdate() {
+        Vector3 move = new Vector3((right-left)*speed,(up-down)*speed,0);
+        GetComponent<Rigidbody2D>().MovePosition(transform.position + move * Time.deltaTime);
+        up=0;
+        down= 0;
+        left=0;
+        right=0;
+        
+
+    }
+    private void GetInput(){
+        if(Input.GetKey(KeyCode.A)){
+            left++;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            right++;
+        }
+        if(Input.GetKey(KeyCode.W)){
+            up++;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            down++;
+        }
+    }
+}
