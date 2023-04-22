f43c033febd758987fd664195f404740a2336720
Mortaza Nalbandi <mortazana8@gmail.com>
2023-04-22 12:46:48 +0330

    added enemy spawner

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+public class EnemySpawn : MonoBehaviour
+{
+    private float rate;
+    private Random rnd;
+
+    public GameObject enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rate = 5;
+        rnd = new Random();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rate<=0){
+            int x =rnd.Next(-24, 24);
+            int y =rnd.Next(-24, 24);
+             Vector3 spawnPos = new Vector3(x,y,0);
+            Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+            rate =5;
+        }
+        rate -= Time.deltaTime;
+    }
+}
