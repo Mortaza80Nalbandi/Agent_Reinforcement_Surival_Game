37df93cda855e9b9bbde960b64ceb001c34086a4
Mortaza Nalbandi <mortazana8@gmail.com>
2023-06-03 08:54:10 +0330

    code cleaned in enemy spawn

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -7,6 +7,11 @@ public class EnemySpawn : MonoBehaviour
     private float rate;
     private Random rnd;
     private int enemies;
+    private int maxEnemies;
+    private int xLowerband;
+    private int yLowerband;
+    private int xHigherband;
+    private int yHigherband;
 
     public GameObject enemyPrefab;
     // Start is called before the first frame update
@@ -14,6 +19,11 @@ public class EnemySpawn : MonoBehaviour
     {
         rate = 10;
         enemies = 1;
+        xLowerband = -24;
+        xHigherband = 24;
+        yLowerband = -24;
+        yHigherband = 24;
+        maxEnemies = 7;
         rnd = new Random();
     }
     public void decreaseEnemies()
@@ -23,10 +33,10 @@ public class EnemySpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rate <= 0 && enemies <= 7)
+        if (rate <= 0 && enemies <= maxEnemies)
         {
-            int x = rnd.Next(-24, 24);
-            int y = rnd.Next(-24, 24);
+            int x = rnd.Next(xLowerband, xHigherband);
+            int y = rnd.Next(yLowerband, yHigherband);
             Vector3 spawnPos = new Vector3(x, y, 0);
             Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
             rate = 2;
