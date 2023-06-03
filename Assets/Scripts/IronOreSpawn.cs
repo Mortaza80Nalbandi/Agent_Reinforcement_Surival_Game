ee415ce37f2f0c2a83cc464dfbd6de05d2b3cd39
Mortaza Nalbandi <mortazana8@gmail.com>
2023-06-03 08:55:23 +0330

    code cleaned in Iron ore spawn

--- Assets/Scripts/IronOreSpawn.cs
+++ Assets/Scripts/IronOreSpawn.cs
@@ -8,10 +8,18 @@ public class IronOreSpawn : MonoBehaviour
     private Random rnd;
 
     public GameObject IOPrefab;
+    private int xLowerband;
+    private int yLowerband;
+    private int xHigherband;
+    private int yHigherband;
     // Start is called before the first frame update
     void Start()
     {
         rate = 10;
+        xLowerband = -24;
+        xHigherband = 24;
+        yLowerband = -24;
+        yHigherband = 24;
         rnd = new Random();
     }
 
@@ -20,8 +28,8 @@ public class IronOreSpawn : MonoBehaviour
     {
         if (rate <= 0)
         {
-            int x = rnd.Next(-24, 24);
-            int y = rnd.Next(-24, 24);
+            int x = rnd.Next(xLowerband, xHigherband);
+            int y = rnd.Next(yLowerband, yHigherband);
             Vector3 spawnPos = new Vector3(x, y, 0);
             Instantiate(IOPrefab, spawnPos, Quaternion.identity);
             rate = 10;
